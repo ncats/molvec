6043d614cb6e63215f35c16255d84ed13a49cd02
Tyler Peryea <tylerperyea@gmail.com>
2019-02-23 18:07:30 -0500

    added chemkit jars temporarily to lib folder

---
+++ mavenInstall.sh
@@ -0,0 +1,4 @@
+mvn install:install-file -Dfile=lib/chemkit-api-0.9.0.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-api -Dversion=0.9.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/chemkit-cdk-0.9.0-jar-with-dependencies.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-cdk -Dversion=0.9.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/chemkit-inchi-0.9.0.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-inchi -Dversion=0.9.0 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/chemkit-renderer-0.9.0.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-renderer -Dversion=0.9.0 -Dpackaging=jar
