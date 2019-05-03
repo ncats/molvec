0079c758a869f21c831c7f79eeeb6c984d048c61
Tyler Peryea <tylerperyea@gmail.com>
2019-05-02 21:51:56 -0400

    fixed dependency problem for chemkit

--- mavenInstall.sh
+++ mavenInstall.sh
@@ -1,3 +1,3 @@
 mvn install:install-file -Dfile=lib/chemkit-api-0.9.2.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-api -Dversion=0.9.2 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/chemkit-cdk-0.9.2.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-cdk -Dversion=0.9.2 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/chemkit-cdk-0.9.2-jar-with-dependencies.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-cdk -Dversion=0.9.2 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/chemkit-renderer-0.9.2.jar -DgroupId=gov.nih.ncats -DartifactId=chemkit-renderer -Dversion=0.9.2 -Dpackaging=jar
