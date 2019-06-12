9265603d567c27e9f55409fcda9b22de51b7b0cb
katzelda <daniel.katzel@nih.gov>
2019-06-12 10:02:25 -0400

    updated to use ncats common packages from maven central and updates readme

--- README.md
+++ README.md
@@ -36,17 +36,66 @@ questions and/or problems to tyler.peryea@nih.gov.
 ## Commandline interface
   The Molvec jar has a runnable Main class with the following options
   
-    usage: molvec [ -f <path> | -gui]
-     -f,--file <arg>   path of image file to process. Supported formats
-                       include png, jpeg, tiff.  This option is required if
-                       not using -gui
-     -gui              Run Molvec in GUI mode. file and scale option may be
-                       set to preload file
-     -h,--help         print usage text
-     -o,--out <arg>    path of output processed mol. Only valid when not using
-                       gui mode. If not specified output is sent to STDOUT
-     -scale <arg>      scale of image to show in viewer (only valid if gui
-                       mode AND file are specified)
+    usage: molvec ([-gui],[[(-f <path> [-o <path>]) | (-dir <path> [-outDir <path>],[-parallel <count>])]],[-scale <value>],[-h])
+    
+    Image to Chemical Structure Extractor Analyzes the given image and tries to find the chemical structure drawn and
+    convert it into a Mol format.
+    
+    options:
+         -dir <path>         path to a directory of image files to process. Supported formats include png, jpeg, tiff. Each
+                             image file found will be attempted to be processed. If -out or -outDir is not specified then
+                             each processed mol will be put in the same directory and named $filename.molThis option or -f
+                             is required if not using -gui
+    
+         -f,--file <path>    path of image file to process. Supported formats include png, jpeg, tiff.  This option or -dir
+                             is required if not using -gui
+    
+         -gui                Run Molvec in GUI mode. file and scale option may be set to preload file
+    
+         -h,--help           print helptext
+    
+         -o,--out <path>     path of output processed mol. Only valid when not using gui mode. If not specified output is
+                             sent to STDOUT
+    
+         -outDir <path>      path to output directory to put processed mol files. If this path does not exist it will e
+                             created
+    
+         -parallel <count>   Number of images to process simultaneously, if not specified defaults to 1
+    
+         -scale <value>      scale of image to show in viewer (only valid if gui mode AND file are specified)
+    
+    Examples:
+    
+          $molvec -f /path/to/image.file
+    
+       parse the given image file and print out the structure mol to STDOUT
+    
+          $molvec -dir /path/to/directory
+    
+       serially parse all the image files inside the given directory and write out a new mol file for each image named
+       $image.file.mol the new files will be put in the input directory
+    
+          $molvec -dir /path/to/directory -outDir /path/to/outputDir
+    
+       serially parse all the image files inside the given directory and write out a new mol file for each image named
+       $image.file.mol the new files will be put in the directory specified by outDir
+    
+          $molvec -dir /path/to/directory -parallel 4
+    
+       parse in 4 concurrent parallel threads all the image files inside the given directory and write out a new mol file for
+       each image named $image.file.mol the new files will be put in the directory specified by outDir
+    
+          $molvec -gui
+    
+       open the Molvec Graphical User interface without any image preloaded
+    
+          $molvec -gui -f /path/to/image.file
+    
+       open the Molvec Graphical User interface  with the given image file preloaded
+    
+          $molvec -gui -f /path/to/image.file -scale 2.0
+    
+       open the Molvec Graphical User interface  with the given image file preloaded zoomed in/out to the given scale
                        
 ### GUI
   Molvec Comes with a Swing Viewer you can use to step
--- src/main/java/gov/nih/ncats/molvec/Main.java
+++ src/main/java/gov/nih/ncats/molvec/Main.java
@@ -1,89 +1,231 @@
 package gov.nih.ncats.molvec;
 
+import gov.nih.ncats.common.cli.Cli;
+import gov.nih.ncats.common.cli.CliSpecification;
+import gov.nih.ncats.common.cli.CliValidationException;
 import gov.nih.ncats.molvec.ui.Viewer;
-import org.apache.commons.cli.*;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import static gov.nih.ncats.common.cli.CliSpecification.*;
 /**
  * Created by katzelda on 5/20/19.
  */
 public class Main {
 
-    public static void main(String[] args){
+    private static class DirectoryProcessor{
+        private int numThreads =1;
 
-        Options options = new Options();
+        private File dir, outputDir;
 
-        options.addOption("h", "help", false,"print usage text");
+        public int getNumThreads() {
+            return numThreads;
+        }
 
-        options.addOption("gui", false, "Run Molvec in GUI mode. file and scale option may be set to preload file");
-        options.addOption("scale", true, "scale of image to show in viewer (only valid if gui mode AND file are specified)");
-
-        options.addOption("f", "file", true,"path of image file to process. Supported formats include png, jpeg, tiff.  This option is required if not using -gui");
-
-        options.addOption("o","out",true, "path of output processed mol. Only valid when not using gui mode. If not specified output is sent to STDOUT");
-
-        CommandLineParser parser = new DefaultParser();
-        try {
-            // parse the command line arguments
-            CommandLine commandLine = parser.parse( options, args );
-            if(commandLine.hasOption("h")){
-                showHelp(options, System.out);
-                return;
+        public void setNumThreads(int numThreads) throws IOException{
+            if(numThreads < 1){
+                throw new CliValidationException("num of threads must be >=1");
             }
+            this.numThreads = numThreads;
+        }
 
-            if(commandLine.hasOption("gui")){
+        public File getDir() {
+            return dir;
+        }
+
+        public void setDir(File dir) throws IOException{
+            if(!dir.exists()){
+                throw new FileNotFoundException("directory '" + dir.getAbsolutePath() + "' does not exist");
+            }
+            this.dir = dir;
+        }
+
+        public File getOutputDir() {
+            return outputDir;
+        }
+
+        public void setOutputDir(File outputDir) throws IOException {
+            if(outputDir !=null){
+                Files.createDirectories(outputDir.toPath());
+            }
+            this.outputDir = outputDir;
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+
+        DirectoryProcessor directoryProcessor = new  DirectoryProcessor();
+
+        CliSpecification spec = CliSpecification.createWithHelp(
+                option("gui").isFlag(true)
+                                    .description("Run Molvec in GUI mode. file and scale option may be set to preload file"),
+                radio(
+                group(option("f").longName("file")
+                        .argName("path")
+                        .description("path of image file to process. Supported formats include png, jpeg, tiff.  This option or -dir is required if not using -gui")
+                        .setRequired(true),
+                        option("o").longName("out")
+                                .argName("path")
+                                .addValidation(cli->!cli.hasOption("gui"), "-o option not valid with neither -dir nor -gui mode")
+                                .description("path of output processed mol. Only valid when not using gui mode. If not specified output is sent to STDOUT")
+
+                        ),
+                        group(
+                        option("dir")
+                                .argName("path")
+                                .description("path to a directory of image files to process. Supported formats include png, jpeg, tiff and svg. " +
+                                        "Each image file found will be attempted to be processed. If -out or -outDir is not specified then " +
+                                        "each processed mol will be put in the same directory and named $filename.mol" +
+                                        "This option or -f is required if not using -gui")
+                                .setToFile(directoryProcessor::setDir)
+                                .setRequired(true),
+                                option("outDir")
+                                    .argName("path")
+                                        .setToFile(directoryProcessor::setOutputDir)
+                                    .description("path to output directory to put processed mol files. If this path does not exist it will e created"),
+                                option("parallel")
+                                        .argName("count")
+                                        .setToInt(directoryProcessor::setNumThreads)
+                                        .description("Number of images to process simultaneously, if not specified defaults to 1")
+
+
+
+                        )),
+                 option("scale")
+                         .argName("value")
+                         .addValidation(cli->cli.hasOption("gui") && cli.hasOption("f"), "scale only valid if specifying file in gui mode")
+                         .description("scale of image to show in viewer (only valid if gui mode AND file are specified)")
+
+
+                )
+        .programName("molvec")
+        .description("Image to Chemical Structure Extractor Analyzes the given image and tries to find the chemical structure drawn and convert it into a Mol format.")
+        .addValidation(cli->cli.hasOption("gui") || cli.hasOption("f") || cli.hasOption("dir"),
+                "-f or -dir option is required if not using -gui mode")
+
+        .example("-f /path/to/image.file", "parse the given image file and print out the structure mol to STDOUT")
+        .example("-dir /path/to/directory", "serially parse all the image files inside the given directory and write out " +
+                        "a new mol file for each image named $image.file.mol the new files will be put in the input directory")
+        .example("-dir /path/to/directory -outDir /path/to/outputDir", "serially parse all the image files inside the given directory and write out " +
+                        "a new mol file for each image named $image.file.mol the new files will be put in the directory specified by outDir")
+        .example("-dir /path/to/directory -parallel 4", "parse in 4 concurrent parallel threads all the image files inside the given directory and write out " +
+                        "a new mol file for each image named $image.file.mol the new files will be put in the directory specified by outDir")
+
+        .example("-gui", "open the Molvec Graphical User interface without any image preloaded")
+        .example("-gui -f /path/to/image.file", "open the Molvec Graphical User interface  with the given image file preloaded")
+
+        .example("-gui -f /path/to/image.file -scale 2.0", "open the Molvec Graphical User interface  with the given image file preloaded zoomed in/out to the given scale")
+
+                .footer("Developed by NIH/NCATS")
+        ;
+
+
+        if(spec.helpRequested(args)){
+            System.out.println(spec.generateUsage());
+            return;
+        }
+        try {
+            Cli cli =spec.parse(args);
+
+            if(cli.hasOption("gui")){
                 //file and scale
-                if(commandLine.hasOption("f")){
+                if(cli.hasOption("f")){
 
-                    String filepath = commandLine.getOptionValue("f");
+                    String filepath = cli.getOptionValue("f");
                     String scale= "1";
-                    if(commandLine.hasOption("scale")){
-                        scale = commandLine.getOptionValue("scale");
+                    if(cli.hasOption("scale")){
+                        scale = cli.getOptionValue("scale");
                     }
                     Viewer.main(new String[]{filepath, scale});
                 }else {
                     Viewer.main(new String[0]);
                 }
-            }else if(commandLine.hasOption("f")){
+            }else if(cli.hasOption("f")){
 
 
-                    String mol = Molvec.ocr(new File(commandLine.getOptionValue("f")));
-                    if(commandLine.hasOption("o")){
-                        File outputFile = new File(commandLine.getOptionValue("o"));
-                        File parent = outputFile.getParentFile();
-                        if(parent !=null){
-                            Files.createDirectories(parent.toPath());
-                        }
-
-                        try(PrintWriter writer = new PrintWriter(new FileWriter(outputFile))){
-                            writer.println(mol);
-                        }
-                    }else{
-                        System.out.println(mol);
+                String mol = Molvec.ocr(new File(cli.getOptionValue("f")));
+                if(cli.hasOption("o")){
+                    File outputFile = new File(cli.getOptionValue("o"));
+                    File parent = outputFile.getParentFile();
+                    if(parent !=null){
+                        Files.createDirectories(parent.toPath());
                     }
+
+                    try(PrintWriter writer = new PrintWriter(new FileWriter(outputFile))){
+                        writer.println(mol);
+                    }
+                }else{
+                    System.out.println(mol);
+                }
+            }else if(cli.hasOption("dir")){
+                File dir = directoryProcessor.getDir();
+
+                File outputDir = directoryProcessor.getOutputDir();
+                if(outputDir ==null){
+                    outputDir = dir;
+                }
+                ExecutorService executorService = Executors.newFixedThreadPool(directoryProcessor.getNumThreads());
+                File files[] =dir.listFiles( f->{
+                    String name = f.getName();
+                    int extOffset = name.lastIndexOf('.');
+                    if(extOffset <0){
+                        return false;
+                    }
+                    String ext = name.substring(extOffset+1);
+                    return "png".equalsIgnoreCase(ext) || "jpg".equalsIgnoreCase(ext) || "jpeg".equalsIgnoreCase(ext)
+                            || "tiff".equalsIgnoreCase(ext);
+                });
+                if(files ==null || files.length ==0){
+                    System.out.println("No image files found");
+                    return;
+                }
+                CountDownLatch latch = new CountDownLatch(files.length);
+                for(File f : files){
+                    executorService.submit(new MolVecRunnable(f, outputDir, latch));
+                }
+                executorService.shutdown();
+                latch.await();
             }else{
                 //invalid
-                throw new ParseException("gui mode or file not specified");
+                throw new CliValidationException("gui mode or file not specified");
             }
-        }catch(ParseException e){
-            System.err.println( "Parsing failed.  Reason: " + e.getMessage() );
-            showHelp(options, System.err);
-        } catch (Exception e) {
-            e.printStackTrace();
+        }catch(CliValidationException e) {
+            System.err.println(e.getMessage());
+            System.err.println("\n\n" + spec.generateUsage());
             System.exit(-1);
         }
+
     }
 
-    private static void showHelp(Options options, PrintStream ps){
-        HelpFormatter formatter = new HelpFormatter();
-        try(PrintWriter writer = new PrintWriter(ps)) {
-            formatter.printHelp(writer, HelpFormatter.DEFAULT_WIDTH, "molvec [ -f <path> | -gui] ", null, options, HelpFormatter.DEFAULT_LEFT_PAD, HelpFormatter.DEFAULT_DESC_PAD, null);
+    private static class MolVecRunnable implements Callable<Void>{
+        File f;
+        File outDir;
+        CountDownLatch latch;
+        MolVecRunnable(File f, File outDir, CountDownLatch latch){
+            this.f =f;
+            this.outDir = outDir;
+            this.latch = latch;
+        }
 
+        @Override
+        public Void call() throws Exception{
+            try {
+                System.out.println(" .."+f.getName());
+                String mol = Molvec.ocr(f);
+                File out = new File(outDir, f.getName() + ".mol");
+                try (PrintWriter writer = new PrintWriter(out)) {
+                    writer.println(mol);
+                }
+                return null;
+            }finally{
+                //wait until the end to decrement latch
+                latch.countDown();
+            }
         }
     }
 }
